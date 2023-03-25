9bd32125e922430b04349b3a275b6c6eedbcf44b
AlinaPrysiazhniuk <114912643+AlinaPrysiazhniuk@users.noreply.github.com>
2023-03-25 10:27:28 +0000

    Deploying to gh-pages from @ AlinaPrysiazhniuk/goit-js-hw-10@6fc326ba52499975e86093fe893cb782fa7429cf 🚀

---
+++ index.5bb008cf.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/#api-endpoints-v3-name").then((t=>{if(!t.ok)throw new Error(t.status);return t})).then((t=>{console.log(t)})).catch((t=>{console.warn(t)}));
+//# sourceMappingURL=index.5bb008cf.js.map
